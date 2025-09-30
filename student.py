8495ead6dac5d9eb1c091122b562ebad27ccdb3e
deepajadhav1107 <deepajadhav2001@gmail.com>
2025-09-30 09:43:07 +0530

    save to master

--- student.py
+++ student.py
@@ -2,8 +2,14 @@ name= input("Enter name:")
 fname= input("Enter Father name:")
 mname= input("Enter name:")
 collage=input("Enter collage name: ")
+
+address=input("Enter Address: ")
+age=int(input("Enter age: "))
+
+
 print(name)
 print(fname)
 print(mname)
 print(collage)
-
+print(address)
+print(age)
