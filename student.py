d28c0091ee76eaf25ca3ec576ce71a49cb7aa791
deepajadhav1107 <deepajadhav2001@gmail.com>
2025-09-30 09:30:31 +0530

    save to main

---
+++ student.py
@@ -0,0 +1,9 @@
+name= input("Enter name:")
+fname= input("Enter Father name:")
+mname= input("Enter name:")
+collage=input("Enter collage name: ")
+print(name)
+print(fname)
+print(mname)
+print(collage)
+
