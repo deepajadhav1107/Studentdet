88a397bd5e05855bfd0af9214a98a46db1e4c26a
Deepa Jadhav <deepajadhav2001@gmail.com>
2025-11-03 15:30:37 +0530

    Update student.py

--- student.py
+++ student.py
@@ -1,11 +1,14 @@
 import sys
 
-if len(sys.argv) != 3:
-    print("Usage: python student.py <name> <rollno>")
-    sys.exit(1)
-
-name = sys.argv[1]
-rollno = sys.argv[2]
-
-print("Student Name:", name)
-print("Roll Number:", rollno)
+if len(sys.argv) == 3:
+    name = sys.argv[1]
+    rollno = sys.argv[2]
+    print("✅ User provided input values:")
+    print("Student Name:", name)
+    print("Roll Number:", rollno)
+else:
+    name = "Deepa"   # Default value
+    rollno = "101"   # Default value
+    print("⚙️ No input given — using default values:")
+    print("Student Name:", name)
+    print("Roll Number:", rollno)
