61ed8dea6c14a2f5c170c9b89fe7e0773c51bf3d
Deepa Jadhav <deepajadhav2001@gmail.com>
2025-11-03 13:15:59 +0530

    Update student.py

--- student.py
+++ student.py
@@ -1,13 +1,18 @@
 import sys
-        # Check if correct number of arguments are given
 
-# sys.argv[0] is always the program name
-script_name = sys.argv[0]
-name = sys.argv[1]
-rollno = sys.argv[2]
+# Check if enough arguments are passed
+# sys.argv[0] = script name
+if len(sys.argv) == 3:
+    name = sys.argv[1]
+    rollno = sys.argv[2]
+else:
+    # Default values (used when no arguments passed)
+    name = "Deepa"
+    rollno = "101"
 
-print("Script Name:", script_name)
+print("=== Student Details ===")
 print("Student Name:", name)
 print("Roll Number:", rollno)
+
 
 
