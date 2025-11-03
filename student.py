5dbd6cec5cc54b874e8d67d813b783d5c1196182
Deepa Jadhav <deepajadhav2001@gmail.com>
2025-11-03 15:31:56 +0530

    Update student.py

--- student.py
+++ student.py
@@ -3,7 +3,7 @@ import sys
 if len(sys.argv) == 3:
     name = sys.argv[1]
     rollno = sys.argv[2]
-    print("✅ User provided input values:")
+    print("User provided input values:")
     print("Student Name:", name)
     print("Roll Number:", rollno)
 else:
