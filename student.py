3e9d439d4a21b5e51e30a41086dc3b34d81bad93
Deepa Jadhav <deepajadhav2001@gmail.com>
2025-11-03 12:05:17 +0530

    Update student.py

--- student.py
+++ student.py
@@ -1,15 +1,7 @@
 name= input("Enter name:")
-fname= input("Enter Father name:")
-mname= input("Enter name:")
-collage=input("Enter collage name: ")
+usn= input("Enter Father name:")
 
-address=input("Enter Address: ")
-age=int(input("Enter age: "))
 
 
 print(name)
-print(fname)
-print(mname)
-print(collage)
-print(address)
-print(age)
+print(usn)
