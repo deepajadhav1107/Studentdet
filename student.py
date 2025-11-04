e25b3793bdac102c9cb63b3dabc9e7c577598a52
Deepa Jadhav <deepajadhav2001@gmail.com>
2025-11-04 10:58:18 +0530

    Update student.py

--- student.py
+++ student.py
@@ -1,9 +1,8 @@
 import sys
 
-file=sys.argv[0]
-name = sys.argv[1]
-rollno = sys.argv[2]
+name = "deepa"
+rollno = "90"
 print("User provided input values:")
-print("file name",file)
+
 print("Student Name:", name)
 print("Roll Number:", rollno)
